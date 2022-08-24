2c03b30acf938a6e185fc49b492d50e83a0b6434
Sanket Ray <iamsanketray123@gmail.com>
2022-08-24 09:19:38 +0530

    Create HammingDistance.swift

---
+++ HammingDistance.swift
@@ -0,0 +1,11 @@
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        var xor = x^y
+        var distance = 0
+        
+        while xor != 0 {
+            distance += 1
+            xor = xor & (xor-1)
+        }
+        
+        return distance
+    }
