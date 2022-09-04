2bbbe2c496879905620af6ea0b98d9e9938f265b
Sanket Ray <iamsanketray123@gmail.com>
2022-09-04 09:13:46 +0530

    Update Queue.swift

--- Queue.swift
+++ Queue.swift
@@ -1,104 +1,57 @@
-// class Node<T> {
-    
-//     var value: T
-//     var next: Node?
-    
-//     init(value: T, next: Node? = nil) {
-//         self.value = value
-//         self.next = next
-//     }
-// }
-
-// struct Queue<T> {
-    
-//     var front: Node<T>?
-    
-//     var rear: Node<T>?
-    
-//     init() { }
-    
-//     var isEmpty: Bool {
-//         return front == nil
-//     }
-    
-//     var peek: T? {
-//         return front?.value
-//     }
-// }
-
-// extension Queue {
-    
-//     mutating private func push(_ value: T) {
-//         front = Node(value: value, next: front)
-//         if rear == nil {
-//             rear = front
-//         }
-//     }
-    
-//     mutating func enqueue(_ value: T) {
-//         if isEmpty {
-//             self.push(value)
-//             return
-//         }
-        
-//         rear?.next = Node(value: value)
-//         rear = rear?.next
-//     }
-    
-//     mutating func dequeue() -> T? {
-//         defer {
-//             front = front?.next
-//             if isEmpty {
-//                 rear = nil
-//             }
-//         }
-//         return front?.value
-//     }
-// }
-
 class Node<T> {
-
-  // MARK: - Variables
-  var value:T
-  var next:Node?
     
-  // MARK: - Initialisation Methods  
-  init(value:T) {
-    self.value = value
-  }
+    var value: T
+    var next: Node?
+    
+    init(value: T, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
 }
 
-class Queue<T> {
+struct Queue<T> {
     
-  // MARK: - Variables  
-  var tail:Node<T>?
-  var head:Node<T>?
-  var count:Int = 0
-  var isEmpty: Bool {
-      return count == 0
-  }
+    var front: Node<T>?
+    
+    var rear: Node<T>?
+    
+    init() { }
+    
+    var isEmpty: Bool {
+        return front == nil
+    }
+    
+    var peek: T? {
+        return front?.value
+    }
+}
 
-  // MARK: - Helper Methods  
-  func dequeue() -> Node<T>? {
-    if let node = head {
-      head = head?.next
-      count -= 1
-      return node
-    }
-    return nil
-  }
+extension Queue {
     
-  func enqueue(_ value:T) {
-    let newNode = Node(value:value)
-    if let tailNode = tail {
-      tailNode.next = newNode
-      newNode.next = nil
-      tail = newNode
-    } else {
-      head = newNode
-      tail = newNode
+    mutating private func push(_ value: T) {
+        front = Node(value: value, next: front)
+        if rear == nil {
+            rear = front
+        }
     }
-    count += 1
-  }
     
+    mutating func enqueue(_ value: T) {
+        if isEmpty {
+            self.push(value)
+            return
+        }
+        
+        rear?.next = Node(value: value)
+        rear = rear?.next
+    }
+    
+    mutating func dequeue() -> T? {
+        defer {
+            front = front?.next
+            if isEmpty {
+                rear = nil
+            }
+        }
+        return front?.value
+    }
 }
