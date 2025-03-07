d2c6ba1a61fba5f56a6869d9a947ba7691a6ef13
Sanket Ray <iamsanketray123@gmail.com>
2025-03-07 15:32:09 +0530

    Update UnionFind.swift

--- UnionFind.swift
+++ UnionFind.swift
@@ -1,41 +1,29 @@
-class UnionFind {
-    
-    // MARK: - Variables
-    var parent: [Int]
+final class UnionFind {
     var rank: [Int]
+    var parent: [Int]
     var count: Int
-    
-    // MARK: - Initialisation Method
+
     init(_ count: Int) {
-        parent = Array(repeating: -1, count: count)
         rank = Array(repeating: 1, count: count)
+        parent = Array(0..<count)
         self.count = count
     }
-    
-    // MARK: - Helper Methods
-    func findParent(_ node: Int) -> Int {
 
-        if parent[node] == -1 {
-            return node
+    func findParent(_ node: Int) -> Int {
+        if parent[node] != node {
+            parent[node] = findParent(parent[node])
         }
-        
-        // Path Compression
-        parent[node] = findParent(parent[node])
-      
         return parent[node]
     }
-    
+
     func union(_ node1: Int, _ node2: Int) {
-        
         let parent1 = findParent(node1)
         let parent2 = findParent(node2)
-        
-        // If both have same parents, there is are part of one component already and hence there is no need for union
+
         if parent1 == parent2 {
             return
         }
-        
-        // Union by rank
+
         if rank[parent1] > rank[parent2] {
             parent[parent2] = parent1
         } else if rank[parent1] < rank[parent2] {
@@ -44,8 +32,7 @@ class UnionFind {
             parent[parent1] = parent2
             rank[parent2] += 1
         }
-        
-        // Reduce count by 1 when we merge two components
+
         count -= 1
     }
 }
