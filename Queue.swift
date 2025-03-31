f0386d476850bccf738689fe0e784900cbb17e6e
Sanket Ray <iamsanketray123@gmail.com>
2025-03-31 22:57:21 +0530

    Update Queue.swift

--- Queue.swift
+++ Queue.swift
@@ -1,5 +1,4 @@
 class Node<T> {
-    
     var value: T
     var next: Node?
     
@@ -11,11 +10,9 @@ class Node<T> {
 
 struct Queue<T> {
     
-    var front: Node<T>?
-    var rear: Node<T>?
-    private(set) var count: Int = 0 // Track the count of elements in the queue
-    
-    init() { }
+    private var front: Node<T>?
+    private var rear: Node<T>?
+    private(set) var count: Int = 0
     
     var isEmpty: Bool {
         return count == 0
@@ -24,46 +21,38 @@ struct Queue<T> {
     var peek: T? {
         return front?.value
     }
-
-    mutating private func push(_ value: T) {
-        front = Node(value: value, next: front)
-        if rear == nil {
-            rear = front
-        }
-        count += 1
-    }
     
     mutating func enqueue(_ value: T) {
+        let newNode = Node(value: value)
         if isEmpty {
-            self.push(value)
-            return
+            front = newNode
+            rear = newNode
+        } else {
+            rear?.next = newNode
+            rear = newNode
         }
-        
-        rear?.next = Node(value: value)
-        rear = rear?.next
         count += 1
     }
     
     mutating func dequeue() -> T? {
         guard let value = front?.value else { return nil }
-
         front = front?.next
         count -= 1
-
+        
         if isEmpty {
-            rear = nil
+            rear = nil  // Reset rear if queue is empty
         }
-
+        
         return value
     }
-
+    
     func printQueue() {
         var current = front
         var result: [T] = []
         
-        while current != nil {
-            result.append(current!.value)
-            current = current?.next
+        while let node = current {
+            result.append(node.value)
+            current = node.next
         }
         
         print(result.map { "\($0)" }.joined(separator: " -> "))
