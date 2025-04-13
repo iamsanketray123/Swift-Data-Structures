c076d2565eea92d51909399e0c1ba1d86d127337
Sanket Ray <iamsanketray123@gmail.com>
2025-04-13 09:48:00 +0530

    Update BinaryExponentiation.swift

--- BinaryExponentiation.swift
+++ BinaryExponentiation.swift
@@ -1,17 +1,12 @@
 func binaryExponentiation(_ base: Int, _ exponent: Int, mod: Int = 1_000_000_007) -> Int {
-    var result = 1
-    var a = base % mod
-    var b = exponent
-    
-    while b > 0 {
-        // If the current power is odd, multiply the result by the base
-        if b % 2 == 1 {
-            result = (result * a) % mod
-        }
-        // Square the base and halve the exponent
-        a = (a * a) % mod
-        b /= 2
+    if exponent == 0 { return 1 }
+
+    let half = binaryExponentiation(base, exponent / 2, mod: mod)
+    let squared = (half * half) % mod
+
+    if exponent % 2 == 0 {
+        return squared
+    } else {
+        return (squared * (base % mod)) % mod
     }
-    
-    return result
 }
